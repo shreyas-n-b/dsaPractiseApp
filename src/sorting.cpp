97ebb947da52f20e2dc56b852d3145f8a9559b12
shreyas-n-b <shreyubiradar@gmail.com>
2025-11-18 18:00:45 +0530

    added bubble sort

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -90,3 +90,22 @@ void quick_sort(vector<int>& arr) {
 }
 
 //QUICK SORT ENDS
+
+//BUBBLE SORT BEGINS
+
+void bubbleSort(vector<int>& arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+//BUBBLE SORT ENDS
